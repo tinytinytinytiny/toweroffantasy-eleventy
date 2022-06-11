c9ff9657a35daaa541fd361d6f060579bc5cf5e2
tinytinytinytiny <tinynametaken@yahoo.com>
2022-06-10 22:22:48 -0400

    prepare to add edge function

--- src/js/damage-calculator-test.js
+++ src/js/damage-calculator-test.js
@@ -1,6 +1,74 @@
 'use strict';
 
-const params = new URLSearchParams(window.location.href.split('?')[1]);
-const inputs = new Map(params);
+document.getElementById('damage-calculator').addEventListener('change', (e) => {
+	console.table({
+		target: e.target.name,
+		value: e.target.value
+	});
+});
 
-console.log(inputs);
+document.getElementById('damage-calculator').addEventListener('reset', (e) => {
+	e.currentTarget.querySelectorAll('[hidden]').forEach((el) => {
+		el.getAttribute('hidden');
+		el.removeAttribute('hidden');
+	});
+});
+
+document.getElementById('weapon-buffs').addEventListener('change', (e) => {
+	const bonusEffects = new Set();
+	[...e.currentTarget.querySelectorAll('optgroup[label="Bonus Effects"] > *')].map(option => bonusEffects.add(option.value));
+
+	const dropdowns = e.currentTarget.querySelectorAll('select');
+	const dropdownsExceptSelected = e.currentTarget.querySelectorAll(
+		[...dropdowns]
+			.map(i => i.name)
+			.filter(i => i !== e.target.name).map(wep => `[name="${wep}"]`)
+	);
+
+	const selections = [...dropdowns]
+		.map(i => stripNumber(i.value))
+		.filter(i => i.length);
+
+	dropdownsExceptSelected.forEach((dropdown) => {
+		const bonusEffectsGroup = dropdown.querySelector(`[label="Bonus Effects"]`);
+
+		dropdown.querySelectorAll('[label="Awakening Buffs"] option').forEach((option) => {
+		  if (selections.includes(stripNumber(option.value)) && stripNumber(option.value) !== stripNumber(dropdown.value)) {
+		  	option.setAttribute('hidden', '');
+		  } else {
+		  	option.getAttribute('hidden');
+				option.removeAttribute('hidden');
+		  }
+		});
+
+		if (bonusEffects.has(e.target.value)) {
+			bonusEffectsGroup.setAttribute('hidden', '');
+		} else {
+			if (!selections.filter(i => bonusEffects.has(i)).length) {
+				bonusEffectsGroup.getAttribute('hidden');
+				bonusEffectsGroup.removeAttribute('hidden');
+			}
+		}
+
+		if (bonusEffects.has(e.target.value) && bonusEffects.has(dropdown.value)) {
+			dropdown.value = '';
+			dropdown.focus();
+		}
+
+		if (e.target.value && stripNumber(dropdown.value) === stripNumber(e.target.value)) {
+			dropdown.value = '';
+			dropdown.focus();
+		}
+	});
+});
+
+const params = new Map(new URLSearchParams(window.location.href.split('?')[1]));
+
+console.log(params);
+
+if (params.size) {
+}
+
+function stripNumber(str) {
+	return str.replace(/[0-9]/g, '');
+}
