2bbaeb09af29e564456255c40ec31a9d9db706a3
tinytinytinytiny <105854913+tinytinytinytiny@users.noreply.github.com>
2022-07-11 15:48:46 -0400

    Update font-sizes.js

--- src/design-tokens/font-sizes.js
+++ src/design-tokens/font-sizes.js
@@ -1,15 +1,15 @@
-// @link https://utopia.fyi/type/calculator?c=320,16,1.2,1800,19.5,1.333,8,2,&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l|l-2xl|s-xl
+// @link https://utopia.fyi/type/calculator?c=320,16,1.2,1280,19.5,1.333,8,2,&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l|l-2xl|s-xl
 
 module.exports = {
 	'step--2': 'clamp(0.69rem, calc(0.70rem + -0.01vw), 0.69rem)',
-	'step--1': 'clamp(0.83rem, calc(0.82rem + 0.09vw), 0.91rem)',
-	'step-0': 'clamp(1.00rem, calc(0.95rem + 0.24vw), 1.22rem)',
-	'step-1': 'clamp(1.20rem, calc(1.11rem + 0.46vw), 1.62rem)',
-	'step-2': 'clamp(1.44rem, calc(1.28rem + 0.78vw), 2.17rem)',
-	'step-3': 'clamp(1.73rem, calc(1.48rem + 1.25vw), 2.89rem)',
-	'step-4': 'clamp(2.07rem, calc(1.69rem + 1.92vw), 3.85rem)',
-	'step-5': 'clamp(2.49rem, calc(1.92rem + 2.86vw), 5.13rem)',
-	'step-6': 'clamp(2.99rem, calc(2.15rem + 4.16vw), 6.84rem)',
-	'step-7': 'clamp(3.58rem, calc(2.39rem + 5.98vw), 9.11rem)',
-	'step-8': 'clamp(4.30rem, calc(2.60rem + 8.49vw), 12.15rem)'
+	'step--1': 'clamp(0.83rem, calc(0.81rem + 0.14vw), 0.91rem)',
+	'step-0': 'clamp(1.00rem, calc(0.93rem + 0.36vw), 1.22rem)',
+	'step-1': 'clamp(1.20rem, calc(1.06rem + 0.71vw), 1.62rem)',
+	'step-2': 'clamp(1.44rem, calc(1.20rem + 1.21vw), 2.17rem)',
+	'step-3': 'clamp(1.73rem, calc(1.34rem + 1.93vw), 2.89rem)',
+	'step-4': 'clamp(2.07rem, calc(1.48rem + 2.96vw), 3.85rem)',
+	'step-5': 'clamp(2.49rem, calc(1.61rem + 4.40vw), 5.13rem)',
+	'step-6': 'clamp(2.99rem, calc(1.70rem + 6.42vw), 6.84rem)',
+	'step-7': 'clamp(3.58rem, calc(1.74rem + 9.22vw), 9.11rem)',
+	'step-8': 'clamp(4.30rem, calc(1.68rem + 13.08vw), 12.15rem)'
 };
