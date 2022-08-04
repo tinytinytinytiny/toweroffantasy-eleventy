478b2be498e50c3e659f43375f7cee49f7174bb8
tinytinytinytiny <tinynametaken@yahoo.com>
2022-08-04 19:19:44 -0400

    cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,14 @@ module.exports = {
 		require('postcss-import-ext-glob'),
 		require('postcss-import'),
 		require('tailwindcss'),
+		require('@fullhuman/postcss-purgecss')({
+			content: [
+				'./src/*.{html,js,njk,md}',
+				'./src/**/*.{html,js,njk,md}'
+			],
+			defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/\!\[\]]+/g) || [],
+			variables: true
+		}),
 		require('cssnano')
   ]
 };
