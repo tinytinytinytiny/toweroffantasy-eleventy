b7b2a6b93b470d55a19f60f471283ad4a4e2ee2b
tinytinytinytiny <tinynametaken@yahoo.com>
2023-01-08 00:33:02 -0500

    cleaner code

--- src/js/damage-calculator.js
+++ src/js/damage-calculator.js
@@ -6,22 +6,19 @@ document.getElementById('damage-calculator').addEventListener('change', (e) => {
 	}
 });
 
-groupDropdowns({
+groupOptionCategory({
 	fieldset: document.getElementById('weapon-buffs'),
-	group: 'optgroup',
-	selector: 'optgroup[label="Bonus Effects"]'
+	label: 'Bonus Effects'
 });
 
 groupDropdowns({
 	fieldset: document.getElementById('weapon-buffs'),
-	group: 'options',
-	selector: '[label="Advancement Buffs"] option'
+	optgroup: 'Advancement Buffs'
 });
 
 groupDropdowns({
 	fieldset: document.getElementById('matrices'),
-	group: 'options',
-	selector: '[label="2-set"] option'
+	optgroup: '2-set'
 });
 
 document.getElementById('matrices').addEventListener('change', (e) => {
@@ -32,13 +29,11 @@ document.getElementById('matrices').addEventListener('change', (e) => {
 			chip2Fieldset.disabled = true;
 
 			e.currentTarget.querySelectorAll('[name*="chip2"]').forEach((input) => {
-				const tagName = input.tagName.toLowerCase();
-
-				if (tagName === 'select') {
+				if (matchesTagName(input, 'select')) {
 					input.value = '';
 				}
 
-				if (tagName === 'input' && input.value === '0') {
+				if (matchesTagName(input, 'input') && input.value === '0') {
 					input.checked = true;
 				}
 			});
@@ -58,129 +53,163 @@ document.getElementById('matrices').addEventListener('change', (e) => {
  *
  * Forces a group of <select> elements to have unique values, like a group of radio buttons with the same name. When the value of a <select> changes, all the options with the same value in the sibling <select> elements are hidden using the 'hidden' attribute.
  *
- * By default, each <option> is made unique; however, by changing 'group' to 'optgroup', an entire <optgroup> can be made unique. For example, if one <select> element’s value is in the Bonus Weapon Effect <optgroup>, all sibling <select> elements are prevented from selecting any weapons under the same <optgroup>, not just the one weapon that was previously selected.
- *
  * @param {HTMLElement} fieldset the parent element that will listen for change events
- * @param {String} group 'options' will make each option unique; 'optgroup' will make an entire optgroup unique
- * @param {String} selector CSS selector for <option> or <optgroup> elements to pass into a querySelector
+ * @param {String} optgroup the 'label' attribute of the <optgroup> of which each <option> element is a child
  */
-function groupDropdowns({ fieldset, group = 'options', selector } = {}) {
-	let optgroupValues;
+function groupDropdowns({ fieldset, optgroup }) {
+	const dropdowns = getDropdowns(fieldset);
+	const selections = dropdowns.getSelectedValues();
 
-	if (group === 'optgroup') {
-		const optgroupChildren = [...fieldset.querySelectorAll(`${selector || optgroup} > *`)];
+	/**
+	 * @function hideSelectedOptions
+	 * 
+	 * Check the <select> element's <option> values and hide any that have already been selected.
+	 * Any <option> elements whose values aren't currently selected will be made visible again.
+	 * 
+	 * @param {Array} selections all the <option> values that are currently selected
+	 * @param {HTMLElement} dropdown the parent <select> element
+	 */
+	function hideSelectedOptions(selections, dropdown) {
+		const options = dropdown.querySelectorAll(`[label="${optgroup}"] option`);
 
-		optgroupValues = new Set();
-		optgroupChildren.forEach(option => optgroupValues.add(option.value));
-	}
-
-	const dropdowns = fieldset.querySelectorAll('select');
-	const selections = [...dropdowns]
-		.map(i => stripNumber(i.value))
-		.filter(i => i.length);
-
-	dropdowns.forEach((dropdown) => {
-		const siblingDropdowns = fieldset.querySelectorAll(
-			[...dropdowns]
-				.map(dropdown => dropdown.name)
-				.filter(name => name !== dropdown.name)
-				.map(name => `[name="${name}"]`)
+		const valueAlreadySelected = (option) => Boolean(
+			selections.includes(stripNumber(option.value))
+			&& stripNumber(option.value) !== stripNumber(dropdown.value)
 		);
 
-		if (group === 'options') {
-			siblingDropdowns.forEach((siblingDropdown) => {
-				const options = siblingDropdown.querySelectorAll(selector || 'option');
+		options.forEach((option) => {
+			if (valueAlreadySelected(option)) {
+				hide(option);
+			} else {
+				unhide(option);
+			}
+		});
+	}
 
-				options.forEach((option) => {
-					if (selections.includes(stripNumber(option.value)) && stripNumber(option.value) !== stripNumber(siblingDropdown.value)) {
-						option.setAttribute('hidden', '');
-						option.setAttribute('disabled', '');
-					} else {
-						option.getAttribute('hidden');
-						option.removeAttribute('hidden');
-						option.getAttribute('disabled');
-						option.removeAttribute('disabled');
-					}
-				});
-			});
-		}
+	dropdowns.getAll().forEach((dropdown) => {
+		const siblingDropdowns = dropdowns.exclude(dropdown);
 
-		if (group === 'optgroup') {
-			siblingDropdowns.forEach((siblingDropdown) => {
-				const optgroup = dropdown.querySelector(selector || 'optgroup');
-				const optgroupChildren = optgroup.querySelectorAll('option');
-
-				if (optgroupValues.has(stripNumber(siblingDropdown.value))) {
-					hide(optgroup);
-					optgroupChildren.forEach((option) => hide(option));
-				} else {
-					if (!selections.filter(i => optgroupValues.has(i)).length) {
-						unhide(optgroup);
-						optgroupChildren.forEach((option) => unhide(option));
-					}
-				}
-			});
-		}
+		siblingDropdowns.forEach((sibling) => hideSelectedOptions(selections, sibling));
 	});
 
 	fieldset.addEventListener('change', (e) => {
-		if (e.target.tagName.toLowerCase() !== 'select' && e.target.tagName.toLowerCase() !== 'option') {
+		if (!matchesTagName(e.target, 'select') && !matchesTagName(e.target, 'option')) {
 			return;
 		}
 
-		const dropdowns = e.currentTarget.querySelectorAll('select');
-		const dropdownsExceptSelected = e.currentTarget.querySelectorAll(
-			[...dropdowns]
-				.map(dropdown => dropdown.name)
-				.filter(name => name !== e.target.name)
-				.map(name => `[name="${name}"]`)
-		);
+		const dropdowns = getDropdowns(e.currentTarget);
+		const dropdownsExceptSelected = dropdowns.exclude(e.target);
+		const selections = dropdowns.getSelectedValues();
 
-		const selections = [...dropdowns]
-			.map(i => stripNumber(i.value))
-			.filter(i => i.length);
+		dropdownsExceptSelected.forEach((dropdown) => {
+			hideSelectedOptions(selections, dropdown);
 
-		if (group === 'options') {
-			dropdownsExceptSelected.forEach((dropdown) => {
-				const options = dropdown.querySelectorAll(selector || 'option');
-
-				options.forEach((option) => {
-					if (selections.includes(stripNumber(option.value)) && stripNumber(option.value) !== stripNumber(dropdown.value)) {
-						hide(option);
-					} else {
-						unhide(option);
-					}
-				});
-
-				if (e.target.value && stripNumber(dropdown.value) === stripNumber(e.target.value)) {
-					dropdown.value = '';
-					dropdown.focus();
-				}
-			});
-		}
-
-		if (group === 'optgroup') {
-			dropdownsExceptSelected.forEach((dropdown) => {
-				const optgroup = dropdown.querySelector(selector || 'optgroup');
-				const optgroupChildren = optgroup.querySelectorAll('option');
-
-				if (optgroupValues.has(stripNumber(e.target.value))) {
-					hide(optgroup);
-					optgroupChildren.forEach((option) => hide(option));
-				} else {
-					if (!selections.filter(i => optgroupValues.has(i)).length) {
-						unhide(optgroup);
-						optgroupChildren.forEach((option) => unhide(option));
-					}
-				}
-
-				if (optgroupValues.has(e.target.value) && optgroupValues.has(dropdown.value)) {
-					dropdown.value = '';
-					dropdown.focus();
-				}
-			});
-		}
+			// in case hideSelectedOptions() fails, clear the values of sibling <select>s if their values matches the current selection
+			if (
+				e.target.value
+				&& stripNumber(dropdown.value) === stripNumber(e.target.value)
+			) {
+				dropdown.value = '';
+				dropdown.focus();
+			}
+		});
 	});
+}
+
+/**
+ * @function groupOptionCategory
+ *
+ * A variant of the groupDropdowns function that makes <optgroup> elements unique.
+ *
+ * For example, if one <select> element’s value is in the Bonus Weapon Effect <optgroup>, all sibling <select> elements are prevented from selecting any weapons under the same <optgroup>, not just the one weapon that was previously selected.
+ *
+ * @param {HTMLElement} fieldset the parent element that will listen for change events
+ * @param {String} label the 'label' attribute of the <optgroup>
+ */
+function groupOptionCategory({ fieldset, label }) {
+	const optgroupChildren = [...fieldset.querySelectorAll(`optgroup[label="${label}"] > *`)];
+	const optgroupValues = new Set();
+
+	optgroupChildren.forEach(option => optgroupValues.add(option.value));
+
+	const dropdowns = getDropdowns(fieldset);
+	const selections = dropdowns.getSelectedValues();
+
+	dropdowns.getAll().forEach((dropdown) => {
+		const siblingDropdowns = dropdowns.exclude(dropdown);
+
+		// check all sibling <select> elements for <optgroup>s that contain <option>s with values that match the current selection
+		siblingDropdowns.forEach((sibling) => {
+			const optgroup = sibling.querySelector(`optgroup[label="${label}"]`);
+			const optgroupChildren = optgroup.querySelectorAll('option');
+
+			if (optgroupValues.has(stripNumber(sibling.value))) {
+				hide(optgroup);
+				optgroupChildren.forEach((option) => hide(option));
+			} else {
+				if (!selections.filter(i => optgroupValues.has(i)).length) {
+					unhide(optgroup);
+					optgroupChildren.forEach((option) => unhide(option));
+				}
+			}
+		});
+	});
+
+	fieldset.addEventListener('change', (e) => {
+		if (!matchesTagName(e.target, 'select') && !matchesTagName(e.target, 'option')) {
+			return;
+		}
+
+		const dropdowns = getDropdowns(e.currentTarget);
+		const dropdownsExceptSelected = dropdowns.exclude(e.target);
+		const selections = dropdowns.getSelectedValues();
+
+		dropdownsExceptSelected.forEach((dropdown) => {
+			const optgroup = dropdown.querySelector(`optgroup[label="${label}"]`);
+			const optgroupChildren = optgroup.querySelectorAll('option');
+
+			if (optgroupValues.has(stripNumber(e.target.value))) {
+				hide(optgroup);
+				optgroupChildren.forEach((option) => hide(option));
+			} else {
+				if (!selections.filter(i => optgroupValues.has(i)).length) {
+					unhide(optgroup);
+					optgroupChildren.forEach((option) => unhide(option));
+				}
+			}
+
+			if (
+				optgroupValues.has(e.target.value)
+				&& optgroupValues.has(dropdown.value)
+			) {
+				dropdown.value = '';
+				dropdown.focus();
+			}
+		});
+	});
+}
+
+function getDropdowns(parent) {
+	const dropdowns = parent.querySelectorAll('select');
+
+	return {
+		getAll: () => dropdowns,
+		getSelectedValues: () =>
+			[...dropdowns]
+				.map(dropdown => stripNumber(dropdown.value))
+				.filter(i => i.length), // filters out blank array entries
+		exclude: (exclusion) =>
+			parent.querySelectorAll(
+				[...dropdowns]
+					.map(dropdown => dropdown.name)
+					.filter(name => name !== exclusion.name)
+					.map(name => `[name="${name}"]`)
+			)
+	};
+}
+
+function matchesTagName(element, tagName) {
+	return Boolean(element.tagName.toLowerCase() === tagName.toLowerCase());
 }
 
 function stripNumber(str) {
